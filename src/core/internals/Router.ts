a30c0ed64d3a4dc85436137ebd18e56fcdc7c03e
Noel <augu@voided.pw>
2020-06-03 17:29:30 -0700

    Name properties better in the base router

--- src/core/internals/Router.ts
+++ src/core/internals/Router.ts
@@ -1,24 +1,28 @@
 import { Logger, ConsoleTransport } from '@augu/logging';
 import { RouteDefinition, Website } from '..';
+import { randomBytes } from 'crypto';
 import { Collection } from '@augu/immutable';
 
 export default class BaseRouter {
   private logger: Logger;
   public website: Website;
   public routes: Collection<RouteDefinition>;
-  public route: string;
+  public prefix: string;
 
-  constructor(website: Website, route: string) {
+  constructor(website: Website, prefix: string) {
     this.website = website;
     this.routes = new Collection();
-    this.logger = new Logger(`Router ${route}`, { transports: [new ConsoleTransport()] });
-    this.route = route;
+    this.logger = new Logger(`Router ${prefix}`, { transports: [new ConsoleTransport()] });
+    this.prefix = prefix;
   }
 
   register(routes: RouteDefinition[]) {
     for (const route of routes) {
-      this.logger.info(`Registering route ${route.path}...`);
-      this.routes.set(route.path, route);
+      const path = route.path === '/' ? this.prefix : `${this.prefix === '/' ? '' : this.prefix}${route.path}`;
+      route.path = path;
+
+      this.routes.set(randomBytes(4).toString('hex'), route);
+      this.logger.info(`Registered route ${path}!`);
     }
   }
 }
