4a300d6067a5a00d601e37038e08f0d8a944d15c
Noel <augu@voided.pw>
2020-06-04 09:41:02 -0700

    Work on GitHub OAuth2

--- src/routes/oauth2.ts
+++ src/routes/oauth2.ts
@@ -1,7 +1,11 @@
+import type { ServerResponse, IncomingMessage } from 'http';
 import { Route, BaseRouter, Website, Method } from '../core';
 import type { FastifyRequest, FastifyReply } from 'fastify';
-import type { ServerResponse } from 'http';
 import FormData from 'form-data';
+
+interface IncomingQuery {
+  code?: string;
+}
 
 type Reply = FastifyReply<ServerResponse>;
 export default class OAuth2Router extends BaseRouter {
@@ -24,7 +28,28 @@ export default class OAuth2Router extends BaseRouter {
 
   //#region GitHub
   @Route('/github', { method: Method.Get })
-  async onGitHub(_: FastifyRequest, res: Reply) {
+  async redirectToGithub(_: FastifyRequest, res: Reply) {
     res.redirect(this.website.config.github.appUrl);
+  }
+
+  @Route('/github/callback', { method: Method.Post })
+  async onCallbackGitHub(req: FastifyRequest<IncomingMessage, IncomingQuery>, res: Reply) {
+    if (!req.query.hasOwnProperty('code')) return res.status(401).send({
+      statusCode: 401,
+      message: 'No `?code` was provided.'
+    });
+
+    const body = new FormData();
+    body.append('client_id', this.website.config.github.clientID);
+    body.append('client_secret', this.website.config.github.clientSecret);
+    body.append('redirect_uri', this.website.config.github.callbackUrls[this.website.config.environment]);
+    body.append('code', req.query.code);
+
+    const access = await this.website.http.request({
+      method: 'POST',
+      url: ''
+    })
+      .body(body)
+      .execute();
   }
 }
