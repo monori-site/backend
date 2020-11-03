6c7e6f1a71fea4dc0266f27289cfae2594de1923
Noel <august@augu.dev>
2020-11-03 01:37:34 -0700

    Some sort of build script working?

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,68 @@
 plugins {
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    kotlin("plugin.serialization") version "1.4.10"
+    id("com.diffplug.spotless") version "5.7.0"
+    id("com.palantir.docker") version "0.25.0"
     kotlin("jvm") version "1.4.10"
+    application
 }
 
+val ver = Version(1, 0, 0)
+
 group = "dev.floofy"
-version = "1.0.0"
+version = ver.string()
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
+kotlin {
+    sourceSets {
+        val main by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+                implementation("com.charleskorn.kaml:kaml:0.25.0")
+                implementation(kotlin("stdlib"))
+            }
+        }
+    }
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = JavaVersion.VERSION_11.toString()
+        }
+    }
+}
+
+class Version(
+    private val major: Int,
+    private val minor: Int,
+    private val revision: Int
+) {
+    fun string(): String = "$major.$minor.$revision"
+    fun commit(): String = exec("git rev-parse HEAD")
+}
+
+/**
+ * Executes a command from the build script to return an output
+ * @param command The command to execute
+ * @return The raw value of the command's output
+ */
+fun exec(command: String): String {
+    val parts = command.split("\\s".toRegex())
+    val process = ProcessBuilder(*parts.toTypedArray())
+        .directory(file("./"))
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start()
+
+    process.waitFor(1, TimeUnit.MINUTES)
+    return process
+        .inputStream
+        .bufferedReader()
+        .readText()
+        .trim()
 }
