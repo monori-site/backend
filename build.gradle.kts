4279b6edaaf85a7bfa0c4e45a3dc043959abbcbf
Noel <august@augu.dev>
2020-11-03 17:21:12 -0700

    Get a working Gradle project :thumbsup:

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,29 @@
+/**
+ * Copyright (c) 2020 August
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.diffplug.gradle.spotless.SpotlessApply
+import org.apache.tools.ant.filters.ReplaceTokens
+
 plugins {
     id("com.github.johnrengelman.shadow") version "6.1.0"
     kotlin("plugin.serialization") version "1.4.10"
@@ -17,16 +43,47 @@ repositories {
     jcenter()
 }
 
-kotlin {
-    sourceSets {
-        val main by getting {
-            dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-                implementation("com.charleskorn.kaml:kaml:0.25.0")
-                implementation(kotlin("stdlib"))
-            }
-        }
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("com.charleskorn.kaml:kaml:0.25.0")
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("io.vertx:vertx-core:3.9.4")
+    implementation("org.koin:koin-core:2.1.6")
+    implementation("io.sentry:sentry:3.1.1")
+    implementation(kotlin("stdlib"))
+}
+
+val metadata = task<Copy>("metadata") {
+    from("src/main/resources") {
+        include("**/metadata.properties")
+
+        val tokens = mapOf(
+                "app.version" to ver.string(),
+                "app.commit" to ver.commit()
+        )
+
+        filter<ReplaceTokens>(mapOf("tokens" to tokens))
     }
+
+    rename { "app.properties" }
+    into("src/main/resources")
+    includeEmptyDirs = true
+}
+
+val spotlessApply: SpotlessApply by tasks
+val shadowJar: ShadowJar by tasks
+
+spotless {
+    kotlin {
+        trimTrailingWhitespace()
+        licenseHeaderFile("${rootProject.projectDir}/.cache/HEADER")
+        endWithNewline()
+    }
+}
+
+application {
+    mainClassName = "dev.floofy.monori.Bootstrap"
 }
 
 tasks {
@@ -34,6 +91,25 @@ tasks {
         kotlinOptions {
             jvmTarget = JavaVersion.VERSION_11.toString()
         }
+    }
+
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        archiveClassifier.set(null as String?)
+        archiveBaseName.set("Monori")
+
+        manifest {
+            attributes(mapOf(
+                    "Manifest-Version" to "1.0.0",
+                    "Main-Class" to "dev.floofy.monori.Bootstrap"
+            ))
+        }
+    }
+
+    build {
+        dependsOn(spotlessApply)
+        dependsOn(shadowJar)
+        dependsOn(metadata)
     }
 }
 
