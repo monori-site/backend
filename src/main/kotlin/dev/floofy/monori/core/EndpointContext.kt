6559bcbd03453fffd3b8d32423024d1de36e4c0b
Noel <august@augu.dev>
2020-11-20 03:15:01 -0700

    request handler?

--- src/main/kotlin/dev/floofy/monori/core/EndpointContext.kt
+++ src/main/kotlin/dev/floofy/monori/core/EndpointContext.kt
@@ -7,22 +7,19 @@ import io.vertx.core.json.JsonObject
 
 class EndpointContext(
     val session: Any?,
-    val params: Map<String, String>,
-    val query: Map<String, String>,
-    val body: JsonObject?,
-    val req: HttpServerRequest,
-    val res: HttpServerResponse
+    val request: HttpServerRequest,
+    val response: HttpServerResponse
 ) {
 
     /**
      * Send an empty payload
      */
-    fun end() = res.setStatusCode(204).end()
+    fun end() = response.setStatusCode(204).end()
 
     /**
      * Closes the response with a status code and a JSON object
      * @param statusCode The status code to use
      * @param body The body to pass down
      */
-    fun send(statusCode: Int, body: JsonObject) = res.setStatusCode(statusCode).end(body)
+    fun send(statusCode: Int, body: JsonObject) = response.setStatusCode(statusCode).end(body)
 }
