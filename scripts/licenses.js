8d540eeca9bc34fcf71cf72e73129cc96482f2bf
Noel <cutie@floofy.dev>
2021-07-19 17:31:27 -0700

    scripts: add licenses.js

--- scripts/licenses.js
+++ scripts/licenses.js
@@ -16,10 +16,62 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+const { readdirSync, statSync, readFileSync } = require('fs');
 const path = require('path');
 const fs = require('fs/promises');
 
-const main = async() => {};
+const main = async() => {
+  const licenseFile = await fs.readFile('../assets/HEADER', { encoding: 'utf8' })
+    .then(l => l.replace('{{present}}', new Date().getFullYear().toString()));
+
+  const projects = [
+    'backend',
+    'frontend'
+  ];
+
+  for (project of projects) {
+    const files = readdir(`../${project}/src`).filter(file => file.endsWith('.ts'));
+    for (file of files) {
+      const contents = readFileSync(file).toString('utf-8');
+      const newContents = [
+        licenseFile,
+        '',
+        ...contents,
+        ''
+      ];
+
+      console.log(`✔ ${file}`);
+      await fs.writeFile(file, newContents.join('\n'));
+    }
+  }
+
+  return 0;
+};
+
+const readdir = (dir) => {
+  let results = [];
+  const files = readdirSync(dir);
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const fullPath = path.join(path, file);
+
+    // doesn't de-reference symbolic links
+    const stats = statSync(fullPath);
+    const isFile = (stats.isFile() || stats.isFIFO());
+
+    // Check if it's a directory
+    if (!isFile) {
+      const files = readdirSync(fullPath, options);
+      results = results.concat(files);
+    } else {
+
+      results.push(fullPath);
+    }
+  }
+
+  return files;
+};
 
 main()
   .then((code) => process.exit(code))
